9c737c8ba10c6fe7a8d20fec4a49509b6c9933eb
riteshrimal <127923734+riteshrimal@users.noreply.github.com>
2024-04-22 10:23:48 +0545

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,30 +1,34 @@
-// public/client.js
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
-    const messagesList = document.getElementById('messages');
-    const name = localStorage.getItem('chatUserName'); // Retrieve the user's name from local storage
+  const socket = io();
+  const messagesList = document.getElementById('messages');
+  const name = localStorage.getItem('chatUserName'); // Retrieve the user's name from local storage
+  const soundPath = 'mixkit-gaming-lock-2848.wav'; // Replace with your sound file path (MP3)
 
-    function sendMessage() {
-        const messageInput = document.getElementById('messageInput');
-        const message = messageInput.value.trim();
+  function sendMessage() {
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value.trim();
 
-        if (message !== '') {
-            socket.emit('message', { name, message }); // Send the message along with the user's name
-            messageInput.value = '';
-        }
+    if (message !== '') {
+      socket.emit('message', { name, message }); // Send message and user name
+      messageInput.value = '';
+
+      // Play pop-up sound on sending message
+      const audio = new Audio(soundPath);
+      audio.play();
     }
+  }
 
-    document.getElementById('sendButton').addEventListener('click', sendMessage);
-    document.getElementById('messageInput').addEventListener('keypress', (event) => {
-        if (event.key === 'Enter') {
-            sendMessage();
-        }
-    });
+  document.getElementById('sendButton').addEventListener('click', sendMessage);
+  document.getElementById('messageInput').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  });
 
-    socket.on('message', (data) => {
-        const li = document.createElement('li');
-        li.textContent = `${data.name}: ${data.message}`;
-        messagesList.appendChild(li);
-        messagesList.scrollTop = messagesList.scrollHeight;
-    });
+  socket.on('message', (data) => {
+    const li = document.createElement('li');
+    li.textContent = `${data.name}: ${data.message}`;
+    messagesList.appendChild(li);
+    messagesList.scrollTop = messagesList.scrollHeight;
+  });
 });
